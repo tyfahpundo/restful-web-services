0fc50be760a109a3043ef46f684d076a790e4592
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-23 23:04:47 +0200

    Added UserControllers

---
+++ src/main/java/zw/co/afrosoft/restfulwebservices/ui/controller/UserController.java
@@ -0,0 +1,43 @@
+package zw.co.afrosoft.restfulwebservices.ui.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import zw.co.afrosoft.restfulwebservices.ui.model.request.UserDetailsRequestModel;
+import zw.co.afrosoft.restfulwebservices.ui.model.response.UserResponse;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1")int page,
+                           @RequestParam(value = "limit", defaultValue = "10")int limit){
+        return "getUser was called";
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse>  getUserById(@PathVariable String userId){
+        UserResponse returnValue = new UserResponse();
+        returnValue.setFirstName("Tafadzwa");
+        returnValue.setLastName("Pundo");
+        returnValue.setEmail("tyfah@gmail.com");
+        returnValue.setUserId("263");
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserDetailsRequestModel userDetails){
+        UserResponse returnValue = new UserResponse();
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        returnValue.setEmail(userDetails.getEmail());
+        return new ResponseEntity<>(returnValue, HttpStatus.OK);
+    }
+    @PutMapping
+    public String updateUser(){
+        return "updateUser was called";
+    }
+
+    public String deleteUser(){
+        return "deleteUser was called";
+    }
+}
