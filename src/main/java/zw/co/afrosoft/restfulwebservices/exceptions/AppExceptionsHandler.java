8ebd37afce576c17eb567157e2f9055124dc6c0c
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-27 12:43:02 +0200

    Return Custom Error Message Object

--- src/main/java/zw/co/afrosoft/restfulwebservices/exceptions/AppExceptionsHandler.java
+++ src/main/java/zw/co/afrosoft/restfulwebservices/exceptions/AppExceptionsHandler.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import zw.co.afrosoft.restfulwebservices.ui.model.response.ErrorMessage;
+
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
         return new ResponseEntity<>(
-                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
